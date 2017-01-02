c5c7a3e7c03566d5ad1bc534773af6dc345a9b86
valoriedodge <valoriejones@gmail.com>
2017-01-02 15:00:06 -0700

    complete

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,36 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    members.delete_if{|line_member| line_member == person}
   end
 
   def front
+    members.first
   end
 
   def middle
+    members[members.length/2]
   end
 
   def back
+    members.last
   end
 
   def search(person)
+    foundperson = index(person)
+    if foundperson
+      members[foundperson]
+    end
   end
 
   private
 
   def index(person)
+    members.index(person)
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,32 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(height){Array.new(width)}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x,y)
+      matrix[x][y] = pixel
+    end
   end
 
   def at(x, y)
+    matrix[x][y]
   end
 
   private
 
   def inbounds(x, y)
+    if (x >=0 && x <=width)
+      if (y >=0 && y <= height)
+        true
+      end
+    else
+      false
+    end
   end
 
 end
