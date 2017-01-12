647bc6589cf9335c7e86008325ad97243ddb07b3
valoriedodge <valoriejones@gmail.com>
2017-01-11 21:53:07 -0800

    benchmark updated

--- 01-data-structures/03-linked-lists/benchmark.rb
+++ 01-data-structures/03-linked-lists/benchmark.rb
@@ -11,36 +11,48 @@ llist = LinkedList.new
   nodeArr[num] = node
 end
 
-Benchmark.bm do |x|
+Benchmark.bmbm do |x|
   x.report("10,000 Array") {
-    (0..n).each do |num|
-      arr[num] = num
+    100.times do
+      (0..n).each do |num|
+        arr[num] = num
+      end
     end
   }
   x.report("10,000 LList") {
-    nodeArr.each do |node|
-      llist.add_to_tail(node)
+    100.times do
+      nodeArr.each do |node|
+        llist.add_to_tail(node)
+      end
     end
   }
   x.report("5,000th Array") {
-    arr[5000]
+    1000.times do
+      arr[5000]
+    end
   }
   x.report("5,000th LList") {
-    current = llist.head
-    5000.times do |i|
-      current = current.next
+    1000.times do
+      current = llist.head
+      5000.times do |i|
+        current = current.next
+      end
     end
   }
   x.report("Delete 5,000th") {
-    arr.delete_at(5000)
+    10.times do
+      arr.delete_at(5000)
+    end
   }
   x.report("Delete 5,000th") {
-    current = llist.head
-    previous = nil
-    5000.times do |i|
-      previous = current
-      current = current.next
+    10.times do
+      current = llist.head
+      previous = nil
+      5000.times do |i|
+        previous = current
+        current = current.next
+      end
+      previous.next = current.next
     end
-    previous.next = current.next
   }
 end
