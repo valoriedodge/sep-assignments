2ee6492a01fb1bf382437e4c03d674ac4257f860
valoriedodge <valoriejones@gmail.com>
2017-02-04 09:07:36 -0700

    min heap

--- 01-data-structures/06-trees/min_heap_tree/min_heap_tree.rb
+++ 01-data-structures/06-trees/min_heap_tree/min_heap_tree.rb
@@ -1,84 +1,224 @@
 require_relative 'node'
-require_relative 'myqueue'
-require_relative 'mystack'
 
 class MinHeapTree
-  attr_accessor :head
+  attr_accessor :tree
 
   def initialize
-    @head = head
+    @tree = []
   end
 
-  def insert(root, node)
-    if @head == nil
-      @head = node
+  def insert(node)
+    if @tree.length < 2
+      @tree[1] = node
       return
     end
-    if @head == root && root.rating < node.rating
-      temp = root
-      @head = node
-      @head.left = temp.left
-      @head.right = temp.right
-      node = temp
-      insert(@head, node)
-      return
-    end
-    current = root
-    queue = MyQueue.new()
-    queue.enqueue(current)
-    while queue.empty?
-      current = queue.dequeue
-      if current.left && current.right
-        if current.left.rating < node.rating
-          temp = current.left
-          current.left = node
-          current.left.left = temp.left
-          current.left.right = temp.right
-          node = temp
-          insert(root, node)
+    index = @tree.length
+    @tree[index] = node
+    rotate_up(index)
+  end
+
+  def find(data)
+    return nil if data.nil?
+    @tree.each { |node| return node if node && node.title == data }
+    return nil
+  end
+
+  def delete(data)
+    return nil if data.nil?
+    @tree.map.with_index do |node, x|
+      if node && node.title == data
+        if x == @tree.length - 1
+          @tree.pop
           return
         end
-        if current.right.rating < node.rating
-          temp = current.right
-          current.right = node
-          current.right.left = temp.left
-          current.right.right = temp.right
-          node = temp
-          insert(root, node)
-          return
+        last = @tree.pop
+        @tree[x] = last
+        parent = x/2
+        if @tree[parent] && @tree[x].rating < @tree[parent].rating
+          rotate_up(x)
+        else
+          rotate_down(x)
         end
-        queue.enqueue(current.left)
-        queue.enqueue(current.right)
-      elsif current.left
-        current.right = node
         return
+      end
+    end
+  end
+
+  def rotate_up(index)
+    parent = index/2
+    while (parent > 0)
+      if @tree[index].rating < @tree[parent].rating
+        temp = @tree[index]
+        @tree[index] = @tree[parent]
+        @tree[parent] = temp
       else
-        current.left = node
         return
       end
+      index = parent
+      parent = index/2
     end
   end
 
-  def delete(root, node)
-    
-  end
-
-  def find(root, node)
-  end
-
-  def printf(children=nil)
-    @head == nil ? return : current = @head
-    queue = MyQueue.new()
-    queue.enqueue(@head)
-    while queue.empty?
-      current = queue.dequeue
-      puts current.title + ": " + current.rating.to_s
-      if current.left != nil
-        queue.enqueue(current.left)
-      end
-      if current.right != nil
-        queue.enqueue(current.right)
+  def rotate_down(index)
+    while (@tree[index*2] != nil)
+      child1 = index*2
+      child2 = (index*2) + 1
+      if @tree[child2] != nil
+        if @tree[index].rating < @tree[child1].rating && @tree[index].rating < @tree[child2].rating
+          return
+        end
+        if @tree[child1].rating < @tree[child2].rating
+          swap(index, child1)
+          index = child1
+        else
+          swap(index, child2)
+          index = child2
+        end
+      else
+        if @tree[index].rating < @tree[child1].rating
+          return
+        else
+          swap(index, child1)
+          index = child1
+        end
       end
     end
   end
+
+  def print
+    @tree.each do |node|
+      if node != nil
+        puts node.title + ": " + node.rating.to_s
+      end
+    end
+  end
+
+  def swap(index1, index2)
+    temp = @tree[index1]
+    @tree[index1] = @tree[index2]
+    @tree[index2] = temp
+  end
+
+
+
+
+#   def insert(root, node)
+#     if @head == nil
+#       @head = node
+#       return
+#     end
+#     if @head == root && root.rating > node.rating
+#       temp = root
+#       @head = node
+#       @head.left = temp.left
+#       @head.right = temp.right
+#       node = temp
+#       insert(@head, node)
+#       return
+#     end
+#     current = root
+#     queue = MyQueue.new()
+#     queue.enqueue(current)
+#     while queue.empty?
+#       current = queue.dequeue
+#       if current.left && current.right
+#         if current.left.rating > node.rating
+#           temp = current.left
+#           current.left = node
+#           current.left.left = temp.left
+#           current.left.right = temp.right
+#           node = temp
+#           insert(root, node)
+#           return
+#         end
+#         if current.right.rating > node.rating
+#           temp = current.right
+#           current.right = node
+#           current.right.left = temp.left
+#           current.right.right = temp.right
+#           node = temp
+#           insert(root, node)
+#           return
+#         end
+#         queue.enqueue(current.left)
+#         queue.enqueue(current.right)
+#       elsif current.left
+#         current.right = node
+#         return
+#       else
+#         current.left = node
+#         return
+#       end
+#     end
+#   end
+#
+#   def delete(root, node)
+#     return nil if data.nil?
+#     # If we are deleting the root we need to reassign @head
+#     if @head == root && root.title == data
+#       if root.left && root.right
+#         if root.right.rating < root.left.rating
+#           right_root = root.right
+#           @head = delete_min_node(root.right)
+#           @head.right = right_root
+#         else
+#           @head = root.right
+#         end
+#         return
+#       elsif root.right
+#         @head = root.right
+#         return
+#       elsif root.left
+#         @head = root.left
+#         return
+#       else
+#         @head = nil
+#         return
+#       end
+#     end
+#     parent = root if root.right && root.right.title == data
+#     parent = root if root.left && root.left.title == data
+#     if parent
+#       if parent.right && parent.right.title == data
+#         delete_right(parent, parent.right)
+#       end
+#       if parent.left && parent.left.title == data
+#         delete_left(parent, parent.left)
+#       end
+#     end
+#     parent = delete(root.right, data) if root.right && parent.nil?
+#     parent = delete(root.left, data) if root.left && parent.nil?
+#     parent ? parent : nil
+#   end
+#
+#   def find(root, node)
+#     return nil if data.nil?
+#     return root if root && root.title == data
+#     found = find(root.right, data) if root.right
+#     found = find(root.left, data) if root.left && found.nil?
+#     found ? found : nil
+#   end
+#
+#   def printf(children=nil)
+#     @head == nil ? return : current = @head
+#     queue = MyQueue.new()
+#     queue.enqueue(@head)
+#     while queue.empty?
+#       current = queue.dequeue
+#       puts current.title + ": " + current.rating.to_s
+#       if current.left != nil
+#         queue.enqueue(current.left)
+#       end
+#       if current.right != nil
+#         queue.enqueue(current.right)
+#       end
+#     end
+#   end
+#
+#   def rotate_right(parent, node)
+#     if node.left && node.right
+#       if node.left.rating < node.right.rating
+#         parent.left
+#     end
+#   end
 end
--- 01-data-structures/06-trees/min_heap_tree/min_heap_tree_spec.rb
+++ 01-data-structures/06-trees/min_heap_tree/min_heap_tree_spec.rb
@@ -5,8 +5,8 @@ require_relative 'min_heap_tree'
 RSpec.describe MinHeapTree, type: Class do
 
 
-  let (:tree) { MinHeapTree.new }
-  let (:root) { Node.new("The Matrix", 87) }
+  let (:heap) { MinHeapTree.new }
+  let (:matrix) { Node.new("The Matrix", 87) }
   let (:pacific_rim) { Node.new("Pacific Rim", 72) }
   let (:braveheart) { Node.new("Braveheart", 78) }
   let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
@@ -19,180 +19,179 @@ RSpec.describe MinHeapTree, type: Class do
   let (:empire) { Node.new("Star Wars: The Empire Strikes Back", 94) }
   let (:mad_max_2) { Node.new("Mad Max 2: The Road Warrior", 98) }
 
-  before do
-    tree.insert(tree.head, root)
-  end
-
   describe "#insert(data)" do
-    it "properly inserts a new node as head" do
-      tree.insert(tree.head, martian)
-      expect(tree.head.title).to eq "The Martian"
+    it "properly inserts a new node in array" do
+      heap.insert(martian)
+      expect(heap.tree[1].title).to eq "The Martian"
     end
 
     it "properly inserts a new node as a left child" do
-      tree.insert(root, pacific_rim)
-      expect(root.left.title).to eq "Pacific Rim"
+      heap.insert(pacific_rim)
+      heap.insert(martian)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[2].title).to eq "The Martian"
     end
 
     it "properly inserts a new node as a right child" do
-      tree.insert(root, braveheart)
-      tree.insert(root, pacific_rim)
-      expect(root.left.title).to eq "Braveheart"
-      expect(root.right.title).to eq "Pacific Rim"
+      heap.insert(pacific_rim)
+      heap.insert(martian)
+      heap.insert(braveheart)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[3].title).to eq "Braveheart"
     end
 
     it "properly inserts a new node as a left-left child" do
-      tree.insert(root, pacific_rim)
-      tree.insert(root, braveheart)
-      tree.insert(root, jedi)
-      expect(root.left.title).to eq "Star Wars: Return of the Jedi"
-      expect(root.right.title).to eq "Braveheart"
-      expect(root.left.left.title).to eq "Pacific Rim"
+      heap.insert(pacific_rim)
+      heap.insert(jedi)
+      heap.insert(braveheart)
+      heap.insert(martian)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[4].title).to eq "The Martian"
     end
 
     it "properly inserts a new node as a left-left child with rearrangement" do
-      tree.insert(root, braveheart)
-      tree.insert(root, pacific_rim)
-      tree.insert(root, jedi)
-      expect(root.right.title).to eq "Braveheart"
-      expect(root.left.title).to eq "Star Wars: Return of the Jedi"
-      expect(root.left.left.title).to eq "Pacific Rim"
+      heap.insert(pacific_rim)
+      heap.insert(martian)
+      heap.insert(jedi)
+      heap.insert(braveheart)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[2].title).to eq "Braveheart"
+      expect(heap.tree[4].title).to eq "The Martian"
     end
 
     it "properly inserts a new node as root node" do
-      tree.insert(root, district)
-      expect(tree.head.title).to eq "District 9"
-      expect(tree.head.left.title).to eq "The Matrix"
+      heap.insert(martian)
+      expect(heap.tree[1].title).to eq "The Martian"
+      heap.insert(pacific_rim)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[2].title).to eq "The Martian"
     end
 
     it "properly inserts a new node as root node and keeps heap structure" do
-      tree.insert(root, inception)
-      tree.insert(root, martian)
-      expect(tree.head.title).to eq "The Martian"
-      expect(tree.head.left.title).to eq "Inception"
-      expect(tree.head.right.title).to eq "The Matrix"
+      heap.insert(jedi)
+      expect(heap.tree[1].title).to eq "Star Wars: Return of the Jedi"
+      heap.insert(braveheart)
+      heap.insert(martian)
+      heap.insert(pacific_rim)
+      expect(heap.tree[1].title).to eq "Pacific Rim"
+      expect(heap.tree[4].title).to eq "Star Wars: Return of the Jedi"
     end
 
 
   end
 
-#   describe "#find(data)" do
-#     it "handles nil gracefully" do
-#       tree.insert(root, empire)
-#       tree.insert(root, mad_max_2)
-#       expect(tree.find(root, nil)).to eq nil
-#     end
-#
-#     it "properly finds a left node" do
-#       tree.insert(root, pacific_rim)
-#       expect(tree.find(root, pacific_rim.title).title).to eq "Pacific Rim"
-#     end
-#
-#     it "properly finds a left-left node" do
-#       tree.insert(root, braveheart)
-#       tree.insert(root, pacific_rim)
-#       expect(tree.find(root, pacific_rim.title).title).to eq "Pacific Rim"
-#     end
-#
-#     it "properly finds a left-right node" do
-#       tree.insert(root, donnie)
-#       tree.insert(root, inception)
-#       expect(tree.find(root, inception.title).title).to eq "Inception"
-#     end
-#
-#     it "properly finds a right node" do
-#       tree.insert(root, district)
-#       expect(tree.find(root, district.title).title).to eq "District 9"
-#     end
-#
-#     it "properly finds a right-left node" do
-#       tree.insert(root, hope)
-#       tree.insert(root, martian)
-#       expect(tree.find(root, martian.title).title).to eq "The Martian"
-#     end
-#
-#     it "properly finds a right-right node" do
-#       tree.insert(root, empire)
-#       tree.insert(root, mad_max_2)
-#       expect(tree.find(root, mad_max_2.title).title).to eq "Mad Max 2: The Road Warrior"
-#     end
-#   end
-#
-#   describe "#delete(data)" do
-#     it "handles nil gracefully" do
-#       expect(tree.delete(root, nil)).to eq nil
-#     end
-#
-#     it "properly deletes a left node" do
-#       tree.insert(root, hope)
-#       tree.delete(root, hope.title)
-#       expect(tree.find(root, hope.title)).to be_nil
-#     end
-#
-#     it "properly deletes a left-left node" do
-#       tree.insert(root, braveheart)
-#       tree.insert(root, pacific_rim)
-#       tree.delete(root, pacific_rim.title)
-#       expect(tree.find(root, pacific_rim.title)).to be_nil
-#     end
-#
-#     it "properly deletes a left-right node" do
-#       tree.insert(root, donnie)
-#       tree.insert(root, inception)
-#       tree.delete(root, inception.title)
-#       expect(tree.find(root, inception.title)).to be_nil
-#     end
-#
-#     it "properly deletes a right node" do
-#       tree.insert(root, district)
-#       tree.delete(root, district.title)
-#       expect(tree.find(root, district.title)).to be_nil
-#     end
-#
-#     it "properly deletes a right-left node" do
-#       tree.insert(root, hope)
-#       tree.insert(root, martian)
-#       tree.delete(root, martian.title)
-#       expect(tree.find(root, martian.title)).to be_nil
-#     end
-#
-#     it "properly deletes a right-right node" do
-#       tree.insert(root, empire)
-#       tree.insert(root, mad_max_2)
-#       tree.delete(root, mad_max_2.title)
-#       expect(tree.find(root, mad_max_2.title)).to be_nil
-#     end
-#   end
-#
-#   describe "#printf" do
-#      specify {
-#        expected_output = "The Matrix: 87\nStar Wars: Return of the Jedi: 80\nStar Wars: A New Hope: 93\nPacific Rim: 72\nInception: 86\nThe Martian: 92\nStar Wars: The Empire Strikes Back: 94\nBraveheart: 78\nThe Shawshank Redemption: 91\nMad Max 2: The Road Warrior: 98\nDistrict 9: 90\n"
-#        tree.insert(root, hope)
-#        tree.insert(root, empire)
-#        tree.insert(root, jedi)
-#        tree.insert(root, martian)
-#        tree.insert(root, pacific_rim)
-#        tree.insert(root, inception)
-#        tree.insert(root, braveheart)
-#        tree.insert(root, shawshank)
-#        tree.insert(root, district)
-#        tree.insert(root, mad_max_2)
-#        expect { tree.printf }.to output(expected_output).to_stdout
-#      }
-#
-#      specify {
-#        expected_output = "The Matrix: 87\nBraveheart: 78\nMad Max 2: The Road Warrior: 98\nPacific Rim: 72\nInception: 86\nDistrict 9: 90\nStar Wars: Return of the Jedi: 80\nThe Shawshank Redemption: 91\nThe Martian: 92\nStar Wars: The Empire Strikes Back: 94\nStar Wars: A New Hope: 93\n"
-#        tree.insert(root, mad_max_2)
-#        tree.insert(root, district)
-#        tree.insert(root, shawshank)
-#        tree.insert(root, braveheart)
-#        tree.insert(root, inception)
-#        tree.insert(root, pacific_rim)
-#        tree.insert(root, martian)
-#        tree.insert(root, jedi)
-#        tree.insert(root, empire)
-#        tree.insert(root, hope)
-#        expect { tree.printf }.to output(expected_output).to_stdout
-#      }
-#   end
+  describe "#find(data)" do
+    it "handles nil gracefully" do
+      heap.insert(empire)
+      heap.insert(mad_max_2)
+      expect(heap.find(nil)).to eq nil
+    end
+
+    it "properly finds a left node" do
+      heap.insert(pacific_rim)
+      heap.insert(hope)
+      expect(heap.find(hope.title).title).to eq "Star Wars: A New Hope"
+    end
+
+    it "properly finds a deep node" do
+      heap.insert(pacific_rim)
+      heap.insert(jedi)
+      heap.insert(braveheart)
+      heap.insert(martian)
+      heap.insert(donnie)
+      heap.insert(hope)
+      heap.insert(empire)
+      heap.insert(shawshank)
+      expect(heap.find(pacific_rim.title).title).to eq "Pacific Rim"
+      expect(heap.find(empire.title).title).to eq "Star Wars: The Empire Strikes Back"
+    end
+
+    it "returns nil if not in heap" do
+      heap.insert(pacific_rim)
+      heap.insert(jedi)
+      expect(heap.find(hope.title)).to eq nil
+    end
+  end
+
+  describe "#delete(data)" do
+    it "handles nil gracefully" do
+      expect(heap.delete(nil)).to eq nil
+    end
+
+    it "properly deletes a left node" do
+      heap.insert(hope)
+      heap.delete(hope.title)
+      expect(heap.find(hope.title)).to be_nil
+    end
+
+    it "properly deletes a node and preserves heap one" do
+      heap.insert(pacific_rim)
+      heap.insert(jedi)
+      heap.insert(braveheart)
+      heap.insert(martian)
+      heap.insert(donnie)
+      heap.insert(hope)
+      heap.insert(empire)
+      heap.insert(shawshank)
+      heap.delete(pacific_rim.title)
+      expect(heap.tree[1].title).to eq "Braveheart"
+      expect(heap.find(pacific_rim)).to be_nil
+    end
+
+    it "properly deletes a node and preserves heap two" do
+      heap.insert(pacific_rim)
+      heap.insert(jedi)
+      heap.insert(braveheart)
+      heap.insert(martian)
+      heap.insert(donnie)
+      heap.insert(hope)
+      heap.insert(empire)
+      heap.insert(shawshank)
+      heap.delete(braveheart.title)
+      expect(heap.tree[3].title).to eq "The Martian"
+      expect(heap.find(pacific_rim)).to be_nil
+    end
+  end
+
+  describe "#print" do
+    specify {
+      expected_output = "Pacific Rim: 72\nStar Wars: Return of the Jedi: 80\nStar Wars: A New Hope: 93\nStar Wars: The Empire Strikes Back: 94\nThe Martian: 92\n"
+      heap.insert( hope)
+      heap.insert( empire)
+      heap.insert( jedi)
+      heap.insert( martian)
+      heap.insert( pacific_rim)
+      expect { heap.print }.to output(expected_output).to_stdout
+    }
+
+     specify {
+       expected_output = "Pacific Rim: 72\nStar Wars: Return of the Jedi: 80\nBraveheart: 78\nDistrict 9: 90\nThe Martian: 92\nStar Wars: A New Hope: 93\nInception: 86\nStar Wars: The Empire Strikes Back: 94\nThe Shawshank Redemption: 91\nMad Max 2: The Road Warrior: 98\n"
+       heap.insert( hope)
+       heap.insert( empire)
+       heap.insert( jedi)
+       heap.insert( martian)
+       heap.insert( pacific_rim)
+       heap.insert( inception)
+       heap.insert( braveheart)
+       heap.insert( shawshank)
+       heap.insert( district)
+       heap.insert( mad_max_2)
+       expect { heap.print }.to output(expected_output).to_stdout
+     }
+
+     specify {
+       expected_output = "Pacific Rim: 72\nStar Wars: Return of the Jedi: 80\nBraveheart: 78\nInception: 86\nDistrict 9: 90\nThe Shawshank Redemption: 91\nThe Martian: 92\nMad Max 2: The Road Warrior: 98\nStar Wars: The Empire Strikes Back: 94\nStar Wars: A New Hope: 93\n"
+       heap.insert( mad_max_2)
+       heap.insert( district)
+       heap.insert( shawshank)
+       heap.insert( braveheart)
+       heap.insert( inception)
+       heap.insert( pacific_rim)
+       heap.insert( martian)
+       heap.insert( jedi)
+       heap.insert( empire)
+       heap.insert( hope)
+       expect { heap.print }.to output(expected_output).to_stdout
+     }
+  end
 end
